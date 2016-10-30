3f74beaff886173397822498a41f327ae0a7153f
Dirk Schumacher <mail@dirk-schumacher.net>
2016-10-30 20:55:50 +0100

    Add skeleton

---
+++ R/as-hxl.R
@@ -0,0 +1,10 @@
+
+#' @export
+as_hxl <- function(x) {
+  UseMethod("as_hxl")
+}
+
+#' @export
+as_hxl.tbl <- function(x) {
+
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(rhxl)
+
+test_check("rhxl")
